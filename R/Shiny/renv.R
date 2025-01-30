855c3e182c4c41d6a47f510f5e218dd9e7b16d65
Pablo Hendrigo Alves de Melo <pablopains@yahoo.com.br>
2025-01-29 22:53:02 -0300

    r

--- R/Shiny/renv.R
+++ R/Shiny/renv.R
@@ -1,3 +1,42 @@
-setdw('c:\\R_temp')
+# library(devtools)
+library(downloader)
+library(dplyr)
+# library(DT)
+# library(jsonlite)
+# library(lubridate)
+# library(measurements)
+# library(plyr)
+library(readr)
+# library(readxl)
+# library(rhandsontable) # tabela editavel
+# library(rnaturalearthdata)
+# library(rvest)
+library(shiny)
+# library(shinydashboard)
+# library(shinydashboardPlus)
+# library(shinyWidgets) # botoes
+# library(sqldf)
+# library(stringr)
+# library(textclean)
+# library(tidyr)
+# library(writexl)
+# library(glue)
+# library(tidyverse)
 
-renv::snapshot()
+library(rmarkdown)
+library(knitr)
+# library(data.table)
+
+
+
+
+
+
+setwd('C:\\fieldBook - github.com\\fieldBook\\R\\Shiny')
+
+
+
+renv::snapshot(packages = c("downloader", "dplyr", "readr", "shiny", "rmarkdown", "knitr"))
+
+
+
